8cfc617ca869dc2981eed54e9a7beac1235c3fdf
Alfredo Cerritos <alcerri@microsoft.com>
2016-07-27 11:18:06 -0400

    - added an adapter
    - added an adapter

--- CognitiveCrmMobile/CognitiveCrmMobile/Adapters/CardListAdapter.cs
+++ CognitiveCrmMobile/CognitiveCrmMobile/Adapters/CardListAdapter.cs
@@ -19,11 +19,40 @@ namespace CognitiveCrmMobile.Adapters
         List<Card> items;
         Activity context;
 
+        public override int Count
+        {
+            get
+            {
+                return items.Count;
+            }
+        }
+
+        public override Card this[int position]
+        {
+            get
+            {
+                return items[position];
+            }
+        }
 
         public CardListAdapter(Activity context, List<Card> items): base()
         {
             this.context = context;
             this.items = items;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return items[position].id;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var item = items[position];
+            if(convertView == null)
+            {
+                convertView = context.LayoutInflater.Inflate(Android.Resource.Layout.)
+            }
         }
     }
 }
